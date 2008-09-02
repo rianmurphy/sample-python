3364dde061f628dcc8dc516413eb040f185ef1c7
Geir Magnusson Jr <geir@pobox.com>
2008-09-02 09:11:28 -0400

    Switch from _init.js to _init.py so this is pure python.
    Needs changes to appserver as of 20080901

---
+++ _init.py
@@ -0,0 +1,17 @@
+
+core.content.forms();
+
+local.models.ModelBase();
+local.models.utils();
+local.models.course();
+local.models.student();
+
+core.core.routes();
+
+routes = Routes();
+
+routes.student = "student";
+routes.add( "students" , "student" , { "extra" : { "action" : "list" } } );
+
+routes.add( "courses" , "course" , { "extra": { "action" : "list" } } );
+
