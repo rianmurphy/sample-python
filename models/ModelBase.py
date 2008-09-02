4a19286e9984d6a0428812e37ef6361c5753fa13
Ethan Glasser-Camp <ethan@10gen.com>
2008-09-02 18:01:40 -0400

    Translate some functionality into a metaclass, just to see if I remember how.

--- models/ModelBase.py
+++ models/ModelBase.py
@@ -1,10 +1,11 @@
-class ModelBase(object):
-    @classmethod
+class ModelMeta(type):
+    def __new__(mcls, name, bases, attrs):
+        cls = super(ModelMeta, mcls).__new__(mcls, name, bases, attrs)
+        return cls
+
     def find(cls, key=None, fields=None):
         return cls._c.find(key, fields)
-    def save(self):
-        return self._c.save(self)
-    @classmethod
+
     def findOne(cls, key="", create=False):
         if key == "" or key == None:
             if create:
@@ -17,6 +18,15 @@ class ModelBase(object):
         if create and o == None:
             o = cls()
         return o
+
+    def modelBaseSetup(cls):
+        cls._c.setConstructor(cls)
+
+class ModelBase(object):
+    __metaclass__ = ModelMeta
+    def save(self):
+        return self._c.save(self)
+
     def remove(self, key=None):
         if key == None:
             key = {}
@@ -24,13 +34,11 @@ class ModelBase(object):
                 return
             key['_id'] = self._id
         return self._c.remove(key)
-    
+
     # TODO This shouldn't be necessary. But when a template goes to print a course
     # It looks for toString and for some reason the bridge doesn't translate that
     # to __str__
     def toString(self):
         return self.__str__()
 
-    @classmethod
-    def modelBaseSetup(cls):
-        cls._c.setConstructor(cls)
+
